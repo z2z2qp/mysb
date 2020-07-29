f6fab90e9bd7294b191f459a64035682d3de9fc4
will <zjbwill@dingtalk.com>
2020-07-29 17:06:21 +0800

    修改 valid 注解类

--- src/main/java/me/will/sb/controller/Controller.java
+++ src/main/java/me/will/sb/controller/Controller.java
@@ -4,11 +4,12 @@ import me.will.sb.common.HttpResult;
 import me.will.sb.model.req.QueryReq;
 import me.will.sb.model.resp.App;
 import me.will.sb.service.SBService;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("sb")
@@ -21,7 +22,7 @@ public class Controller {
     }
 
     @PostMapping("query")
-    public HttpResult<App> query(@RequestBody @Validated QueryReq req){
+    public HttpResult<App> query(@RequestBody @Valid QueryReq req){
         var app = service.query(req);
         return HttpResult.OK(app);
     }
